cf352e9a8311d16672546f3a0972ac510691daf4
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-04-03 15:16:41 +0530

    basics

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-import requests as rq
+from bs4 import BeautifulSoup
 
-url = "https://timesofindia.indiatimes.com/technology/tech-tips/how-to-create-ghibli-style-anime-art-using-google-gemini-for-free/articleshow/119754131.cms"
+with open("./sample.html", "r") as f:
+    content = f.read()
+    # There are 4 objects in BeautifulSoup
+    # 1. Soup Object(The whole parsed html file)
+    # soup = BeautifulSoup(content, "html.parser")
+    # print(soup)
 
-def fetchUrlAndSaveToFile(url, path):
-    r = rq.get(url)
+    # 2. Tag similar to html tag, but returns the first tag from the entire doc
+    # tag = BeautifulSoup(content, "html.parser")
+    
+    # manipulating tag.name
+    # t = tag.div  # give the first div tag
+    # print(t)
+    # print(t.name)
+    # print(type(t))
+    # t.name = "a"  # changing the name of the tag
+    # print(t)
 
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(r.text)
+    # manipulating tag.attributes
+    # print(t)
+    # print(t.attrs)
+    # print(t["class"])
+    # t["class"] = "Sex"
+    # print(t)
+    # del t["class"]
+    # print(t)
 
+    # 3. Navigable String
+    # st = BeautifulSoup(content, "html.parser")
+    # d = st.div
+    # print(d.string)
+    # print(type(d.string))
+    # print(d.getText())
+    # print(type(d.getText()))
 
-fetchUrlAndSaveToFile(url, "data/times.html") 
+    # 4. Comment Object
+    st = BeautifulSoup(content, "html.parser")
+    d = st.div
+    comment = d.string
+    print(type(comment))
